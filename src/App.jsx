2b68b22070bf85ecac62c564569b9122c27e9733
Bach <dangthobach@gmail.com>
2024-01-30 17:10:21 +0700

    no message

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from './layouts/Navbar/Navbar'
+import './App.css'
+import Content from './layouts/Content/Content'
+
+function App() {
+
+  return (
+    <>
+      <div className="app">
+          <Navbar/>
+          <Content/>
+      </div>
+    </>
+  )
+}
+
+export default App
